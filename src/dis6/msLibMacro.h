f6faf3fda55eea7fa7669baad525d864b1bd6b28
Peter Major <peter.major@cae.com>
2021-01-04 18:22:07 +0100

    Make package buildable

--- src/dis6/msLibMacro.h
+++ src/dis6/msLibMacro.h
@@ -1,7 +1,7 @@
 #ifndef MSLIBMACRO_H
 #define MSLIBMACRO_H
 #if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
-#  ifdef EXPORT_LIBRARY
+#  ifdef OPENDIS_EXPORTS
 #    define EXPORT_MACRO __declspec(dllexport)
 #  else
 #    define EXPORT_MACRO  __declspec(dllimport)
---
+++ src/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma warning (disable: 4091 4100 4244 4251 4267 4275 4305)
+#include "dis6/msLibMacro.h"
+#include <vector>
+#include <map>
+#include <iostream> 
+#include <ostream> 
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <cstring>
